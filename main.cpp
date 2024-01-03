cb4bdeaec0e38e04ad21b1a9f1e0d30222b42430
defr010101 <defr010101@yandex.ru>
2024-01-04 02:06:58 +0300

    Добавить функции для арифметических операций
    - Функция сложения целых чисел
    - Функция вычитания целых чисел
    - Функция умножения целых чисел
    - Функция деления целых чисел

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+int sum(int a, int b) {
+  return a + b;
+}
+
+int subtract(int a, int b) {
+  return a - b;
+}
+
+int multiplication(int a, int b) {
+  return a * b;
+}
+
+int division(int a, int b) {
+  return a / b;
+}
+
 int main() {
   int x = 5;
-  std::cout << x << std::endl;
+  int y = 20;
+  std::cout << subtract(x, y) << std::endl;
   return 0;
 }
